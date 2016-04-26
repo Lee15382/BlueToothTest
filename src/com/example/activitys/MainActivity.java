4b70099b886df7600d7565d92d4d6cbd84995ce4
Lee15382 <983436190@qq.com>
2016-04-26 10:03:22 +0800

    first commit

---
+++ src/com/example/activitys/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.activitys;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.bluetoothbroadcast.DeviceReceiver;
+import com.example.bluetoothtest.R;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private BluetoothAdapter blueAdapter;
+	private DeviceReceiver mydevice;
+	private ArrayAdapter<String> adapter;
+	private List<String> deviceList = new ArrayList<String>();
+	private ListView deviceListView;
+	private Button start;
+	private Button findBlue;
+	private boolean hasregister = false;
+
+	@Override
+	protected void onStart() {
+		// 注册蓝牙接收广播
+		if (!hasregister) {
+			hasregister = true;
+			mydevice = new DeviceReceiver(deviceList);
+			IntentFilter filterStart = new IntentFilter(
+					BluetoothDevice.ACTION_FOUND);
+			IntentFilter filterEnd = new IntentFilter(
+					BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+			registerReceiver(mydevice, filterStart);
+			registerReceiver(mydevice, filterEnd);
+		}
+		super.onStart();
+	}
+	//解除广播
+	@Override
+	protected void onDestroy() {
+		if (blueAdapter != null && blueAdapter.isDiscovering()) {
+			blueAdapter.cancelDiscovery();
+		}
+		if (hasregister) {
+			hasregister = false;
+			unregisterReceiver(mydevice);
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	private void initView() {
+		// 绑定listview
+		deviceListView = (ListView) findViewById(R.id.devicelist);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, deviceList);
+		deviceListView.setAdapter(adapter);
+		// 绑定按钮元素
+		start = (Button) findViewById(R.id.start);
+		findBlue = (Button) findViewById(R.id.findBlue);
+		start.setOnClickListener(this);
+		findBlue.setOnClickListener(this);
+	}
+
+	// 按钮点击事件
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.start:
+			setBluetooth();
+			break;
+		case R.id.findBlue:
+			findAvalibleDevice();
+		default:
+			break;
+		}
+	}
+
+	private void setBluetooth() {
+		// 获取蓝牙适配器
+		blueAdapter = BluetoothAdapter.getDefaultAdapter();
+		// 检查是否获得
+		if (blueAdapter != null) {
+			// 获得蓝牙，蓝牙没有开启，则请求开启
+			if (!blueAdapter.isEnabled()) {
+				Intent intent = new Intent(
+						BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(intent, RESULT_FIRST_USER);
+				// 使蓝牙设备可见，方便配对
+				Intent in = new Intent(
+						BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+				in.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 200);
+				startActivity(in);
+				// 直接开启，不经过提示
+				blueAdapter.enable();
+			}
+		} else {
+			// 设备不支持蓝牙,显示弹出框提醒用户
+			AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+			dialog.setTitle("No bluetooth devices");
+			dialog.setMessage("Your equipment does not support bluetooth, please change device");
+			dialog.setNegativeButton("cancel",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+
+						}
+					});
+			dialog.show();
+		}
+	}
+
+	private void findAvalibleDevice() {
+		// 法获得已经绑定的蓝牙设备列表
+		Set<BluetoothDevice> device = blueAdapter.getBondedDevices();
+
+		if (blueAdapter != null && blueAdapter.isDiscovering()) {
+			deviceList.clear();
+			adapter.notifyDataSetChanged();
+		}
+		if (device.size() > 0) { // 存在已经配对过的蓝牙设备
+			for (Iterator<BluetoothDevice> it = device.iterator(); it.hasNext();) {
+				BluetoothDevice btd = it.next();
+				deviceList.add(btd.getName() + '\n' + btd.getAddress());
+				adapter.notifyDataSetChanged();
+			}
+		} else { // 不存在已经配对过的蓝牙设备
+			deviceList.add("No can be matched to use bluetooth");
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+}
---
+++ src/com/example/bluetoothbroadcast/DeviceReceiver.java
@@ -0,0 +1,36 @@
+package com.example.bluetoothbroadcast;
+
+import java.util.List;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeviceReceiver extends BroadcastReceiver {
+
+	private List<String> deviceList;
+	public DeviceReceiver(List<String> deviceList){
+		this.deviceList = deviceList;
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		String action = intent.getAction();
+		if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+			BluetoothDevice btd = intent
+					.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+			//获得绑定状态，获取相关返回数据，BOND_BONDED=12表示已经绑定
+			if (btd.getBondState() != BluetoothDevice.BOND_BONDED) {
+				deviceList.add(btd.getName() + '\n' + btd.getAddress());
+			}
+
+		}
+	}
+	
+	public List<String> getDeviceList(){
+		return deviceList;
+	}
+
+}
